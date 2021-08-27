0f7dd1d930540b54f2e0e4a986955cf63ada53d0
unknown <por_25418@hotmail.com>
2021-08-27 13:35:13 +0700

    create function for checking prime number and fibonacci number

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+
+const checkPrime = (num) => {
+  if (num < 2) return false;
+  for (let k = 2; k < num; k++) {
+    if (num % k === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+function isPerfectSquare(x) {
+  let s = parseInt(Math.sqrt(x));
+  return s * s === x;
+}
+
+function checkFibonacci(n) {
+  return isPerfectSquare(5 * n * n + 4) || isPerfectSquare(5 * n * n - 4);
+}
 
 function App() {
+  const [inputNum, setInputNum] = useState("");
+  const [selectedType, setSelectedType] = useState("isPrime");
+
+  const handleChangeInputNum = (e) => {
+    const inputVal = e.target.value;
+    if (Number(inputVal) < 0) {
+      setInputNum(1);
+    } else {
+      setInputNum(inputVal);
+    }
+  };
+
+  const handleChangeSelectedType = (e) => {
+    setSelectedType(e.target.value);
+  };
+
+  const renderAnswer = () => {
+    if (selectedType === "isPrime") {
+      return checkPrime(inputNum) ? "true" : "false";
+    } else {
+      if (inputNum) {
+        return checkFibonacci(inputNum) ? "true" : "false";
+      } else {
+        return "false";
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid-container">
+        <div className="grid-item">
+          <input
+            type="number"
+            value={inputNum}
+            onChange={handleChangeInputNum}
+          />
+        </div>
+        <div className="grid-item">
+          <select value={selectedType} onChange={handleChangeSelectedType}>
+            <option value="isPrime">isPrime</option>
+            <option value="isFibonacci">isFibonacci</option>
+          </select>
+        </div>
+        <div className="grid-item">{renderAnswer()}</div>
+      </div>
     </div>
   );
 }
